a1757ac613a003e57b776245aadc84bdf920bc7d
Denis Kovalov <dekovalov@gmail.com>
2023-05-16 23:59:48 +0300

    Implemented the search logic with UI

--- lib/widgets/internet_connection_error.dart
+++ lib/widgets/internet_connection_error.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:weather/components/button.dart';
+import 'package:weather/screens/search_page.dart';
 
 import '../utilities/constants.dart';
 
@@ -21,7 +22,10 @@ class InternetError extends StatelessWidget {
                 Container(
                   width: 140,
                   height: 140,
-                  decoration: const BoxDecoration(shape: BoxShape.circle, color: Color(0xffefefef)),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xffefefef),
+                  ),
                   child: const Icon(
                     Icons.signal_wifi_statusbar_connected_no_internet_4,
                     size: 80,
@@ -32,19 +36,30 @@ class InternetError extends StatelessWidget {
                 Text(
                   AppLocalizations.of(context).noInternet,
                   textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: buttonColor),
+                  style: const TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: buttonColor,
+                  ),
                 ),
                 const SizedBox(height: 40),
                 Padding(
-                  padding: EdgeInsets.only(left: 20, right: 20),
+                  padding: const EdgeInsets.only(left: 20, right: 20),
                   child: Text(
                     textAlign: TextAlign.center,
                     AppLocalizations.of(context).turnOn,
-                    style: TextStyle(color: darkGray, fontSize: 18),
+                    style: const TextStyle(color: darkGray, fontSize: 18),
                   ),
                 ),
                 const SizedBox(height: 80),
-                MyButton(AppLocalizations.of(context).tryAgain, () async {}),
+                MyButton(AppLocalizations.of(context).tryAgain, () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SearchPage(),
+                    ),
+                  );
+                }),
                 const SizedBox(height: 30),
                 const SizedBox(height: 30)
               ],
